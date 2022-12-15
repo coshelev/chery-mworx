98977ab5d057a8851a38f3f1ffb44a7403c9c052
coshelev <coshelev@gmail.com>
2022-12-15 16:01:46 +0300

    2

--- chery.php
+++ chery.php
@@ -1,12 +1,13 @@
 <?php
-$rest_json = file_get_contents("php://input");
+$message = file_get_contents("php://input");
 file_put_contents ("koshelev.txt", $rest_json, FILE_APPEND);
 
-$_POST = json_decode($rest_json, true);
-file_put_contents ("koshelev.txt", $_POST, FILE_APPEND);
+//$_POST = json_decode($rest_json, true);
+//file_put_contents ("koshelev.txt", $_POST, FILE_APPEND);
 
-$message = print_r($_POST,true);
-file_put_contents ("koshelev.txt", $message, FILE_APPEND);
+//$message = print_r($_POST,true);
+//file_put_contents ("koshelev.txt", $message, FILE_APPEND);
+
 
 $data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <request> <phone>'.$_POST["phone"].'</phone>  <site>online.chery-luidor.ru</site> <extra>'.$message.'</extra> </request>';
 file_put_contents ("koshelev.txt", $data, FILE_APPEND);
