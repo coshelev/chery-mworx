ad8ece1f3b56e05ffc065da76c3e18d49a450f5f
coshelev <coshelev@gmail.com>
2022-12-17 12:01:02 +0300

    Final working version

--- chery.php
+++ chery.php
@@ -1,21 +1,21 @@
 <?php
-$message = file_get_contents("php://input");
-file_put_contents ("koshelev.txt", $rest_json, FILE_APPEND);
 
-//$_POST = json_decode($rest_json, true);
+$rest_json = file_get_contents("php://input");
+//file_put_contents ("koshelev.txt", $rest_json , FILE_APPEND);
+
+$_POST = json_decode($rest_json, true);
 //file_put_contents ("koshelev.txt", $_POST, FILE_APPEND);
 
-//$message = print_r($_POST,true);
+$message = $rest_json;
 //file_put_contents ("koshelev.txt", $message, FILE_APPEND);
 
-
 $data = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> <request> <phone>'.$_POST["phone"].'</phone>  <site>online.chery-luidor.ru</site> <extra>'.$message.'</extra> </request>';
-file_put_contents ("koshelev.txt", $data, FILE_APPEND);
+//file_put_contents ("koshelev.txt", $data, FILE_APPEND);
 
-//$url = 'http://mainappl.main.luidorauto.ru/sys_agr/hs/webhooks/callback/v1';
-//$ch = curl_init();
-//curl_setopt($ch,CURLOPT_URL, 	    $url);
-//curl_setopt($ch,CURLOPT_POST, 	    true);
-//curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
-//$result = curl_exec($ch);
+$url = 'http://mainappl.main.luidorauto.ru/sys_agr/hs/webhooks/callback/v1';
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL, 	    $url);
+curl_setopt($ch,CURLOPT_POST, 	    true);
+curl_setopt($ch,CURLOPT_POSTFIELDS, $data);
+$result = curl_exec($ch);
 ?>
